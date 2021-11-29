8b0c5613715e34ad4fe9c355ee66545c7d5623f5
Szabi Vojtek <szabi.vojtek@gmail.com>
2021-11-29 14:27:07 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Clock
+const clock = document.querySelector(".clock");
+
+// Calculator Display
+const display = document.querySelector(".display");
+
+// Calculator Keys
+const clearBtn = document.querySelector("[data-clear]");
+const operationBtn = document.querySelectorAll("[data-operand]");
+const numberBtn = document.querySelectorAll("[data-number]");
+const equalsBtn = document.querySelector("[data-equals]");
+
+// Create function to clear the display
+
+// Create function that checks what operand is clicked and acts accordingly
+// if an operand is clicked clear the display for the new numbers, if an operand
+// is clicked a second time, make the calculation
+
+// Create function to update the display
